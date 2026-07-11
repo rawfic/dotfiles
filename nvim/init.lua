3b9d833062c1aae99c961983242381b36c9ea034
rawfic <wintiges@proton.me>
2026-07-11 03:04:54 -0600

    Basic nvim setup

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,1 +1,66 @@
+local opt = vim.opt
 
+opt.number = true
+opt.relativenumber = true
+
+opt.tabstop = 4
+opt.shiftwidth = 4
+opt.expandtab = true
+opt.smartindent = true
+
+opt.mouse = "a"
+opt.termguicolors = true
+
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
+vim.opt.rtp:prepend(lazypath)
+
+
+require("lazy").setup({
+  spec = {
+      {"folke/tokyonight.nvim", lazy = false, priority = 1000, opts = {},},
+      {'nvim-treesitter/nvim-treesitter', lazy = false, build = ':TSUpdate'},
+      {'nvim-telescope/telescope.nvim', version = '*',
+        dependencies = {
+            'nvim-lua/plenary.nvim',
+            { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },}
+      },
+
+  },
+  install = { colorscheme = { "habamax" } },
+  checker = { enabled = true },
+})
+
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+
+require('nvim-treesitter').install { 'rust', 'javascript', 'zig' }
+
+vim.api.nvim_create_autocmd("FileType", {
+  callback = function()
+    pcall(vim.treesitter.start)
+  end,
+})
+
+vim.cmd[[colorscheme tokyonight-night]]
+
+vim.cmd [[
+  hi Normal guibg=NONE ctermbg=NONE
+  hi EndOfBuffer guifg=bg guibg=bg
+]]
